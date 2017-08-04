17ef21cb9bd4148d9fd9492fa76929ec2ff82ea8
i307942 <xiaoliang.shi@sap.com>
2017-08-04 15:35:41 +0800

    code clean

--- src/observer/BumpPlayer.js
+++ src/observer/BumpPlayer.js
@@ -7,28 +7,43 @@ import Timer from '../util/Timer';
 import vertShader from '../glsl/bump.vert';
 import fragShader from '../glsl/bump.frag';
 
+import shadowVertShader from '../glsl/shadow.vert';
+import shadowFragShader from '../glsl/shadow.frag';
+
 import normalSamplerPath from '../assets/20_2.gif';
 import wallSamplerPath from '../assets/20_1.gif';
 import vertData from '../assets/data';
 
 export default class BumpPlayer {
     constructor (canvas, bumpHeight, useBump) {
-        this._useBump = false;
-        this._bumpHeight = Number(bumpHeight);
-        this._useBump = useBump || true;
-        this._uniform = {
-            uPMatrix: Matrix.perspective(45, canvas.width / canvas.height, 0.1, 100.0)
-        };
         this._state = {
-            xRot: 0,
-            yRot: 0,
-            zRot: 0
+            useBump: useBump || true,
+            bumpHeight: Number(bumpHeight),
+            rectangle: {
+                rotate: {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                },
+                uniform: {
+                    uPMatrix: Matrix.perspective(45, canvas.width / canvas.height, 0.1, 100.0)
+                }
+            }
         };
         this.timer = new Timer(3);
         this.timer.addTask(this.onFrameEnter.bind(this));
 
         let shader = this._shader = Shader.create(canvas, vertShader, fragShader);
         this.initShader(shader);
+
+        this._shadowShader = Shader.create(canvas, shadowVertShader, shadowFragShader);
+        var rectangleMesh = this._rectangleMesh = this._shadowShader.createMesh();
+        rectangleMesh.addAttributeBuffer('aVertexPosition', [
+            -3.0, 0.0, 3.0,
+            -3.0, 0.0, -3.0,
+            3.0, 0.0, 3.0,
+            3.0, 0.0, -3.0
+        ], 3);
     }
 
     initShader (shader) {
@@ -50,12 +65,12 @@ export default class BumpPlayer {
         });
 
         Texture.linear(shader.gl, normalSamplerPath, (texture) => {
-            this._uniform.uNormalSampler = texture;
+            this._state.rectangle.uniform.uNormalSampler = texture;
             this.timer.start();
         });
 
         Texture.linear(shader.gl, wallSamplerPath, (texture) => {
-            this._uniform.uSampler = texture;
+            this._state.rectangle.uniform.uSampler = texture;
             this.timer.start();
         });
     }
@@ -69,21 +84,23 @@ export default class BumpPlayer {
     }
 
     useBump (value) {
-        this._useBump = value;
+        this._state.useBump = value;
     }
 
     bumpHeight (value) {
-        this._bumpHeight = Number(value);
+        this._state.bumpHeight = Number(value);
     }
 
     onFrameEnter (elapse) {
-        this._state.xRot += elapse * 45;
-        this._state.yRot += elapse * 45;
-        this._state.zRot += elapse * 45;
+        var state = this._state;
+        var rectangle = state.rectangle;
+        var rot = rectangle.rotate;
+        rot.x += elapse * 45;
+        rot.y += elapse * 45;
+        rot.z += elapse * 45;
 
         var shader = this._shader;
-        var state = this._state;
-        var uniform = this._uniform;
+        var uniform = rectangle.uniform;
 
         var gl = shader.gl;
 
@@ -93,15 +110,20 @@ export default class BumpPlayer {
 
         var mvMatrix = Matrix.translate(0.0, 0.0, -8.0);
 
-        mvMatrix.rotate(state.xRot, 1, 0, 0);
-        mvMatrix.rotate(state.yRot, 0, 1, 0);
-        mvMatrix.rotate(state.zRot, 0, 0, 1);
+        mvMatrix.rotate(rot.x, 1, 0, 0);
+        mvMatrix.rotate(rot.y, 0, 1, 0);
+        mvMatrix.rotate(rot.z, 0, 0, 1);
 
         uniform.uMVMatrix = mvMatrix;
         uniform.uNormalMatrix = Matrix.transpose(Matrix.inverse(Matrix.clone(mvMatrix)));
-        uniform.uBumpHeight = this._bumpHeight;
-        uniform.bUseBumpMapping = this._useBump;
+        uniform.uBumpHeight = state.bumpHeight;
+        uniform.bUseBumpMapping = state.useBump;
 
         shader.uniforms(uniform).draw(this._mesh, gl.TRIANGLES);
+
+        this._shadowShader.uniforms({
+            uPMatrix: uniform.uPMatrix,
+            uMVMatrix: Matrix.translate(0.0, -2.0, -8.0)
+        }).draw(this._rectangleMesh, gl.TRIANGLE_STRIP);
     }
 };
